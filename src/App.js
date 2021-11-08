d0c4089776b0f6fd167857fe6338762666f56b1e
Unchaptered <workstation19961002@gmail.com>
2021-11-08 13:51:56 +0900

    useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
-      <h1>Welcome Our App</h1>
-      <Button text="Button"/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
-};
+}
 
 export default App;
